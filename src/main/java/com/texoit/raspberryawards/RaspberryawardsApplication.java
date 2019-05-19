3cf5ae095d58a1fa6aece46fdffd5d381081d004
Tiéris Valente <tierisvalente@gmail.com>
2019-05-19 14:31:18 -0300

    First Commit
    Project Springboot Created

---
+++ src/main/java/com/texoit/raspberryawards/RaspberryawardsApplication.java
@@ -0,0 +1,13 @@
+package com.texoit.raspberryawards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryawardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspberryawardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/texoit/raspberryawards/RaspberryawardsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.texoit.raspberryawards;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RaspberryawardsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
